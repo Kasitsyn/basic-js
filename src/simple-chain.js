f716e10b5624c366e9008d9ed69457f387b9c9a7
kasitsyn <kasitsyn@gmail.com>
2022-05-02 23:24:48 +0700

    feat: 8

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    !!arguments ? this.chain.push(`( ${value} )`) : this.chain.push('( )')
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+
+
+  removeLink(position) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    if (Number.isInteger(position) && 0 < position && position <= this.getLength()) {
+      this.chain = [...this.chain.slice(0, position - 1), ...this.chain.slice(position)]
+      return this;
+    } else {
+      this.chain = []
+      throw new Error(`You can't remove incorrect link!`)
+    }
+
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    this.chain.reverse()
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    const final = this.chain.join('~~')
+    this.chain = []
+    return final
+
   }
-};
+}
+
 
 module.exports = {
   chainMaker
--- src/transform-array.js
+++ src/transform-array.js
@@ -11,12 +11,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+ * `--discard-next` исключает следующий за ней элемент исходного массива из преобразованного массива.
+ * `--discard-prev` исключает предшествующий ей элемент исходного массива из преобразованного массива.
+ * `--double-next` дублирует следующий за ней элемент исходного массива в преобразованном массиве.
+ * `--double-prev` дублирует предшествующий ей элемент исходного массива в преобразованном массиве.
+*/
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+
+
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!")
+
+  if (arr.length === 0) return []
+
+  let temp = [...arr]
+
+  temp.forEach((elem, index) => {
+    switch (elem) {
+      case `--discard-next`:
+        index === temp.length - 1 ? temp.splice(index, 1) : temp.splice(index, 2)
+        break;
+      case `--discard-prev`:
+        index === 0 ? temp.splice(index, 1) : temp.splice(index - 1, 2)
+        break;
+      case `--double-next`:
+        index === temp.length - 1 ? temp.splice(index, 1) : temp.splice(index, 1, temp[index + 1])
+        
+        break;
+      case `--double-prev`:
+        index === 0 ? temp.splice(index, 1) : temp.splice(index, 1, temp[index - 1])
+        
+        break;
+
+      default:
+
+        break;
+    }
+  })
+
+  console.log(temp)
+  return temp
+
+
 }
+
+transform(['--discard-next', 1, '--discard-prev', 2, 3, 4, 5])
 
 module.exports = {
   transform
